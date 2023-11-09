c438eff6289100edaa289854e44a8bdca7987b2b
Denis Dubkov <d.dubkov@vk.team>
2023-11-09 10:34:46 +0300

    vite config eslint

--- mini-app/vite.config.ts
+++ mini-app/vite.config.ts
@@ -1,21 +1,20 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import { createHtmlPlugin } from 'vite-plugin-html'
-
+import react from '@vitejs/plugin-react';
+import {defineConfig} from 'vite';
+import {createHtmlPlugin} from 'vite-plugin-html';
 
 export default defineConfig(() => {
   return {
     plugins: [
-      react({ include: 'pathToAllReactFiles.{jsx,tsx}' }),
+      react({include: 'pathToAllReactFiles.{jsx,tsx}'}),
       createHtmlPlugin({
         template: 'public/index.html',
         entry: 'src/index.tsx',
         verbose: true,
-      })
+      }),
     ],
     server: {
       port: 10888,
       open: true,
     },
-  }
-})
+  };
+});
