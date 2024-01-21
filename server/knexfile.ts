756ef1b1385e8437d150486cf38d67ed1618aa51
Denis Dubkov <d.dubkov@vk.team>
2024-01-21 17:12:06 +0300

    migration

---
+++ server/knexfile.ts
@@ -0,0 +1,24 @@
+import 'dotenv/config';
+import type {Knex} from 'knex';
+
+const config: {[key: string]: Knex.Config} = {
+  development: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      user: process.env.DB_USER,
+      database: process.env.DB_NAME,
+      password: process.env.DB_PASSWORD,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: './src/database/migrations',
+    },
+  },
+};
+
+module.exports = config;
---
+++ server/src/database/migrations/20240121114914_users.ts
@@ -0,0 +1,14 @@
+import type {Knex} from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('users', (table) => {
+    table.integer('vk_id').primary();
+    table.increments('serial_number');
+    table.integer('invited_count');
+    table.specificType('parent_ids', 'INT[]');
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('users');
+}
